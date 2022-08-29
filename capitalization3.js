26096416e8e3e8c9852b0bad3d0a30d302d59e4b
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-08-29 16:23:47 +0200

    done

---
+++ capitalization3.js
@@ -0,0 +1,19 @@
+const initialName = "peTDASGSDGeasdasfar";
+
+firstName = initialName.substring(0, 2);
+capitalName = initialName[2].toUpperCase();
+restName = initialName.substring(3);
+// console.log(capitalName);
+// funnyName2 = funnyName1[2].toUpperCase();
+console.log(firstName.toLowerCase() + capitalName + restName.toLowerCase());
+
+// console.log();
+// middleName = magician.substring(
+//   magician.indexOf(" "),
+//   magician.lastIndexOf(" ")
+// );
+// lastName = magician.substring(magician.lastIndexOf(" "));
+
+// console.log("The first name is " + firstName);
+// console.log("The middle name is " + middleName);
+// console.log("The last name is " + lastName);
