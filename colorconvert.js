e90ead0e319b53a1139bbda56c2cc707f934f587
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-09-05 15:11:02 +0200

    color conversion

---
+++ colorconvert.js
@@ -0,0 +1,75 @@
+// const initialName = "Abd ulha mid";
+
+// firstName = initialName.substring(0, 2);
+// firstArray = initialName.split("");
+
+// console.log(firstArray[3]);
+// console.log();
+// capitalName = initialName[2];
+// restName = initialName.substring(3);
+// // console.log(capitalName);
+// // funnyName2 = funnyName1[2].toUpperCase();
+// console.log(
+//   firstName.toLowerCase() + capitalName.toUpperCase() + restName.toLowerCase()
+// );
+
+// capitalize("dfgnkj");
+// capitalize("joe cocker");
+
+// function capitalize(str) {
+//   console.log(str[0].toUpperCase() + str.substring(1));
+// }
+
+getNameParts("robert kj orsdsd");
+function getNameParts(fullname) {
+  firstName = fullname.substring(fullname.indexOf(""), fullname.indexOf(" "));
+  middleName = fullname.substring(
+    fullname.indexOf(" "),
+    fullname.lastIndexOf(" ")
+  );
+
+  lastName = fullname.substring(fullname.lastIndexOf(" "));
+  if (middleName == "") {
+    middleName = "undefined";
+  }
+  console.log({ firstName, middleName, lastName });
+}
+
+// fullName("Potter", "Harry", "James");
+// fullName("Potter", "Harry");
+// fullName("Harry", "Potter");
+// fullName("Potter", "Harry", "James", "Pottypotpot");
+// function fullName(lastName, firstName, middleName) {
+//   if (middleName === undefined) {
+//     console.log(firstName + " " + lastName);
+//   } else {
+//     console.log(firstName + " " + middleName + " " + lastName);
+//   }
+// }
+
+cssToRGB("rgb(192, 13, 1)");
+cssToRGB("rgb(2, 23, 146)");
+function cssToRGB(string) {
+  r = string
+    .substring(string.indexOf("("), string.indexOf(","))
+    .replaceAll("(", "");
+  g = string.substring(string.indexOf(" "), string.lastIndexOf(",")).trim();
+  b = string
+    .substring(string.lastIndexOf(" "), string.indexOf(")"))
+    .replaceAll(")", "")
+    .trim();
+  rNumber = parseInt(r);
+  gNumber = parseInt(g);
+  bNumber = parseInt(b);
+  console.log(rNumber + " " + gNumber + " " + bNumber);
+}
+// console.log();
+// middleName = magician.substring(
+//   magician.indexOf(" "),
+//   magician.lastIndexOf(" ")
+// );
+// lastName = magician.substring(magician.lastIndexOf(" "));
+
+// console.log("The first name is " + firstName);
+// console.log("The middle name is " + middleName);
+// console.log("The last name is " + lastName);
